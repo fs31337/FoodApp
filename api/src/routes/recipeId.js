f4700bfabb064ed4cfc690e9da8f7dad5c4797b6
Franco Salina <francodamiansalina@gmail.com>
2021-06-07 20:08:45 -0300

    async en types

--- api/src/routes/recipeId.js
+++ api/src/routes/recipeId.js
@@ -4,14 +4,20 @@ const { Router } = require('express');
 const router = Router();
 const {KEY} = process.env;
 
+//Esta ruta debe mostrar
+// Los campos mostrados en la ruta principal para cada receta (imagen, nombre, tipo de plato y tipo de dieta)
+// Resumen del plato
+// Puntuación
+// Nivel de "comida saludable"
+// Paso a paso
 
 
 router.get('/:id', function(req, res){
     const {id} = req.params;
-    axios.get(`https://api.spoonacular.com/recipes/${id}/information?includeNutrition=true&apiKey=${KEY}`)
+    axios.get(`https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false&apiKey=${KEY}`)
     .then(response => {
         res.send(response.data)
-    }).catch(err => console.log('ID no encontrado en API', err) )
+    }).catch(err => console.log('ID no encontrado en API', err))
 });
 
 module.exports = router;
--- api/src/routes/types.js
+++ api/src/routes/types.js
@@ -1,17 +1,12 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const {Diet_type} = require('../db.js');
+const { Diet_type } = require("../db.js");
 
-
-
-
-
-router.get('/', async function(req, res){
-    let typesArray = [];
-     await Diet_type.findAll({attributes:['name']}).then(types =>
-         types.map(type => typesArray.push(type.dataValues.name)))
-    //  res.send("Types",{types}))
-     res.send(typesArray);
+router.get("/", async function (req, res) {
+  let typesArray = [];
+  const typesRaw = await Diet_type.findAll({ attributes: ["name"] })
+  typesRaw.map((type) => typesArray.push(type.dataValues.name))
+  res.send(typesArray);
 });
 
 module.exports = router;
