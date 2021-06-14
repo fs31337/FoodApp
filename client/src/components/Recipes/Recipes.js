e7c3c864909a2788e2134155b852dac5dde7e40f
Franco Salina <francodamiansalina@gmail.com>
2021-06-14 18:03:56 -0300

    search filter

--- client/src/components/Recipes/Recipes.js
+++ client/src/components/Recipes/Recipes.js
@@ -1,22 +1,47 @@
 import React, { useState, useEffect } from "react";
 import Recipe from "../Recipe/Recipe";
-import Pagination from './Pagination';
+import Pagination from "./Pagination";
 import "./Recipes.css";
 
 function Recipes({ recipes }) {
-    // const [recipeList,setRecipesList] = useState(recipes);
-    // const [loading,setLoading] = useState(false);
-    const [currentPage,setCurrentPage] = useState(1);
-    const [recipesPerPage,setRecipesPerPage] = useState(2);
+  // const [recipeList,setRecipesList] = useState(recipes);
+  // const [loading,setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recipesPerPage, setRecipesPerPage] = useState(2);
+  const [search, setSearch] = useState("");
 
-    const indexOfLastRecipe = currentPage  * recipesPerPage;
-    const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
-    const currentRecipes = recipes.slice(indexOfFirstRecipe,indexOfLastRecipe);
-    //change page
-    const paginate = (pageNumber) => setCurrentPage(pageNumber)
+  //SearchFilter
+  function filteredRecipes(recipes) {
+    if(search.length === 0){
+       return recipes
+    }
+    const filtered = recipes.filter(recipe => recipe.title.toLowerCase().includes(search.toLowerCase()));
+    return filtered;
+  };
+  const onSearchChange = (e) => {
+    setCurrentPage(1);
+    setSearch(e.target.value)
+  }
+  //Paginate
+  console.log(filteredRecipes(recipes),"filtered recipessss")
+  const indexOfLastRecipe = currentPage * recipesPerPage;
+  const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
+  const currentRecipes = filteredRecipes(recipes).slice(
+    indexOfFirstRecipe,
+    indexOfLastRecipe
+  ); //slice de recipes antes
+  //Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div className="recipe-container">
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search Recipe..."
+        value={search}
+        onChange={onSearchChange}
+      />
       {Array.isArray(currentRecipes) ? (
         currentRecipes.map((recipe) => (
           <Recipe
@@ -29,7 +54,11 @@ function Recipes({ recipes }) {
       ) : (
         <h1>Cargando...</h1>
       )}
-      <Pagination recipesPerPage={recipesPerPage} totalRecipes={recipes.length} paginate={paginate}/>
+      <Pagination
+        recipesPerPage={recipesPerPage}
+        totalRecipes={recipes.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
