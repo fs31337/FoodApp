b8bf069796cc3303ffad6c8e729283d03846b0bf
Franco Salina <francodamiansalina@gmail.com>
2021-06-16 17:31:05 -0300

    recipe card ready

--- client/src/components/Recipe/Recipe.js
+++ client/src/components/Recipe/Recipe.js
@@ -6,9 +6,11 @@ function Recipe({title,id,img,diet}) {
     return (
         <div className="recipe" key={id}>
             <img src={img} alt="imagen"></img>
-            <Link className="link" to={`/recipes/${id}`}>{<h1>{title}</h1>}</Link>
-            <div className="separador"></div>
-            <h3>{diet}</h3>
+            <div className="recipe-text">
+                <Link className="recipe-text__link" to={`/recipes/${id}`}>{<h1>{title}</h1>}</Link>
+                <div className="recipe-text__separador"></div>
+                <h3>{diet.join(", ")}</h3>
+            </div>
         </div>
     )
 }
