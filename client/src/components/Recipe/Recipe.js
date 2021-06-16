81389dd6cffcc804d37547e8c4cd9f0e23258e01
Franco Salina <francodamiansalina@gmail.com>
2021-06-16 16:45:44 -0300

    recipes card design

--- client/src/components/Recipe/Recipe.js
+++ client/src/components/Recipe/Recipe.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
-import "./Recipe.css";
+import "./Recipe.scss";
 
 function Recipe({title,id,img,diet}) {
     return (
         <div className="recipe" key={id}>
             <img src={img} alt="imagen"></img>
-            <Link to={`/recipes/${id}`}>{<h1>{title}</h1>}</Link>
+            <Link className="link" to={`/recipes/${id}`}>{<h1>{title}</h1>}</Link>
+            <div className="separador"></div>
             <h3>{diet}</h3>
         </div>
     )
